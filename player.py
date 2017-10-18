aa7b864d801c31ce6334e536089283421a2be2f3
David Sampson <davidc.sampson@ntlworld.com>
2017-10-18 21:31:48 +0100

    Given the player starting quest log

--- player.py
+++ player.py
@@ -1,5 +1,6 @@
 from items import *
 from map import rooms
+from questlog import log, tip
 
 name = ["", ""]
 
@@ -7,3 +8,6 @@ inventory = [item_id, item_laptop, item_money]
 
 # Start game at the reception
 current_room = rooms["Reception"]
+
+quest_log = log()
+quest_log.add_tip(tip("You should head to your desk to pick up your assignments.", "Your subconscious", "9:59"))

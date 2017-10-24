aa8aea898bde647a045981486ab6077fe78fc165
JPreston-2 <32934986+JPreston-2@users.noreply.github.com>
2017-10-24 12:00:49 +0100

    updated player.py to players.py

---
+++ players.py
@@ -0,0 +1,40 @@
+from items import *
+from map import rooms
+from questlog import *
+
+player = {
+    "name": ["Kirill" , "Sidorov"],
+
+    "inventory" : ["gun"],
+
+    "description" :
+    """Do we want a description?""",
+
+    "health": 250,
+    "ammo" : 3, 
+}
+
+bodyguard = {
+    "name": ["Frank" , "Guard"],
+
+    "inventory" : ["gun"],
+
+    "description" :
+    """Do we want a description?""",
+
+    "health": 250,
+    "ammo" : 3, 
+}
+
+
+# Start game at the reception
+current_room = rooms["Reception"]
+
+current_time = basic_time(13, 49)
+quest_log = log()
+
+start_tip = create_tip_from_file(1, "Your subconscious", current_time)
+quest_log.add_tip(start_tip)
+
+
+
